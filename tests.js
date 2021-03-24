3c518e2a59a90d794eb7bb6a925efff70a0963b3
Lex Zavala <lex.zavala7@gmail.com>
2021-03-24 18:27:33 -0500

    Updated the exercises

--- tests.js
+++ tests.js
@@ -21,12 +21,15 @@ describe("sayHello", function (){
     it('should return a string when called', function () {
         expect(typeof sayHello()).toBe("string");
     });
-    // it('should return the number 3 when passed the argument of the number 2', function () {
-    //     expect(sayHello(2)).toBe(3);
-    // });
-    // it('should return the number -43 when passed the argument of the num -44', function () {
-    //     expect(sayHello(-44)).toBe(-43);
-    // });
+    it('should return "Hello, Jane!" when passed Jane as an argument', function () {
+        expect(sayHello('Jane')).toBe('Hello, Jane!');
+    });
+    it('should return "Hello, Alex!" when passed Alex as an argument', function () {
+        expect(sayHello('Alex')).toBe('Hello, Alex!');
+    });
+    it('should return "Hello, Pat!" when passed Pat as an argument', function () {
+        expect(sayHello('Pat')).toBe('Hello, Pat!');
+    });
     // it('should return the number -2.14 when passed the argument of the number -3.14', function () {
     //     expect(sayHello(-3.14)).toBe(-2.14);
     // });
